6af67bf2b271796b20f3a13242a4dc58dc3a4db2
Omar Khattab <omar.khattab7@icloud.com>
2024-12-25 07:51:23 +0200

    Add XML doc to Base repo

--- Repositories/BaseRepository.cs
+++ Repositories/BaseRepository.cs
@@ -5,17 +5,34 @@ using System.Linq.Expressions;
 
 namespace E_LearningPlatform.Repositories
 {
+    /// <summary>
+    /// A base repository class that provides common data access methods for entities.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
     public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
         private readonly AppDBContext _context;
         private readonly ILogger<BaseRepository<T>> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseRepository{T}"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        /// <param name="logger">The logger instance.</param>
+        /// <exception cref="ArgumentNullException">Thrown when context or logger is null.</exception>
         public BaseRepository(AppDBContext context, ILogger<BaseRepository<T>> logger)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
+        /// <summary>
+        /// Adds a new entity to the database.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>The added entity.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when entity is null.</exception>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while adding the entity.</exception>
         public T Add(T entity)
         {
             try
@@ -34,6 +51,12 @@ namespace E_LearningPlatform.Repositories
             }
         }
 
+        /// <summary>
+        /// Deletes an entity from the database.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        /// <exception cref="ArgumentNullException">Thrown when entity is null.</exception>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while deleting the entity.</exception>
         public void Delete(T entity)
         {
             try
@@ -51,6 +74,13 @@ namespace E_LearningPlatform.Repositories
             }
         }
 
+        /// <summary>
+        /// Finds an entity that matches the specified predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate to match.</param>
+        /// <returns>The entity that matches the predicate.</returns>
+        /// <exception cref="NotFoundException">Thrown when no entity is found that matches the predicate.</exception>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while finding the entity.</exception>
         public T Find(Expression<Func<T, bool>> predicate)
         {
             try
@@ -74,6 +104,11 @@ namespace E_LearningPlatform.Repositories
             }
         }
 
+        /// <summary>
+        /// Gets all entities from the database.
+        /// </summary>
+        /// <returns>An enumerable collection of entities.</returns>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while retrieving the entities.</exception>
         public IEnumerable<T> GetAll()
         {
             try
@@ -88,6 +123,13 @@ namespace E_LearningPlatform.Repositories
             }
         }
 
+        /// <summary>
+        /// Updates an existing entity in the database.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        /// <returns>The updated entity.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when entity is null.</exception>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while updating the entity.</exception>
         public T Update(T entity)
         {
             try
@@ -106,6 +148,11 @@ namespace E_LearningPlatform.Repositories
             }
         }
 
+        /// <summary>
+        /// Gets the count of all entities in the database.
+        /// </summary>
+        /// <returns>The count of all entities.</returns>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while counting the entities.</exception>
         public int Count()
         {
             try
@@ -119,6 +166,12 @@ namespace E_LearningPlatform.Repositories
             }
         }
 
+        /// <summary>
+        /// Gets the count of entities that match the specified criteria.
+        /// </summary>
+        /// <param name="criteria">The criteria to match.</param>
+        /// <returns>The count of entities that match the criteria.</returns>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while counting the entities.</exception>
         public int Count(Expression<Func<T, bool>> criteria)
         {
             try
@@ -132,6 +185,14 @@ namespace E_LearningPlatform.Repositories
             }
         }
 
+        /// <summary>
+        /// Finds all entities that match the specified criteria, with optional pagination.
+        /// </summary>
+        /// <param name="criteria">The criteria to match.</param>
+        /// <param name="skip">The number of entities to skip.</param>
+        /// <param name="take">The number of entities to take.</param>
+        /// <returns>An enumerable collection of entities that match the criteria.</returns>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while finding the entities.</exception>
         public IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int? skip, int? take)
         {
             try
@@ -153,6 +214,11 @@ namespace E_LearningPlatform.Repositories
             }
         }
 
+        /// <summary>
+        /// Saves the changes made to the context.
+        /// </summary>
+        /// <returns>The number of state entries written to the database.</returns>
+        /// <exception cref="RepositoryException">Thrown when an error occurs while saving changes.</exception>
         public int SaveChanges()
         {
             try
@@ -172,15 +238,29 @@ namespace E_LearningPlatform.Repositories
         }
     }
 
-    // Custom exceptions
+    /// <summary>
+    /// Represents errors that occur during repository operations.
+    /// </summary>
     public class RepositoryException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RepositoryException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
         public RepositoryException(string message, Exception innerException = null)
             : base(message, innerException) { }
     }
 
+    /// <summary>
+    /// Represents errors that occur when an entity is not found.
+    /// </summary>
     public class NotFoundException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public NotFoundException(string message) : base(message) { }
     }
 }
